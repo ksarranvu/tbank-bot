d52e4119eed54071f414c101267b37123ed57aef
ksarranvu <5hzsd9fv9y@privaterelay.appleid.com>
2026-07-31 17:34:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,18 @@
+import os
+import telebot
+
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+bot.delete_webhook()
+
 @bot.message_handler(content_types=['photo'])
-def get_file_id(message):
+def get_id(message):
     file_id = message.photo[-1].file_id
-    bot.send_message(message.chat.id, f"file_id:\n`{file_id}`", parse_mode="Markdown")
+    bot.reply_to(message, f"Вот file_id:\n\n`{file_id}`", parse_mode="Markdown")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Пришли мне картинку")
+
+print("Бот запущен")
+bot.infinity_polling()
